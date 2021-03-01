fd514189c61bb2eda59845408b519547fd597e77
Douglas Lima <douglasproglima@gmail.com>
2021-02-28 22:07:23 -0300

    Initial Project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Button } from './components/Button';
+
+function App() {
+  return (
+    <>
+      <h1>Hello Pomodoro</h1>
+      <Button color="red">
+        Button 01
+      </Button>
+      <Button color="green">
+        Button 02
+      </Button>
+      <Button color="blue">
+        Button 03
+      </Button>
+    </>
+  );
+}
+
+export default App;
