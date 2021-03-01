a2ce10c1fbc8c0779783d7aaff6ce8ef2b2bf92b
Douglas Lima <douglasproglima@gmail.com>
2021-03-01 20:22:22 -0300

    Relembrando o Conceito de Estado no React

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
+
 interface ButtonProps {
   color: string;
   children: string;
 }
 export function Button(props: ButtonProps) {
+  const [counter, setCounter] = useState(1);
+
+  function increment(){
+    setCounter(counter + 1);
+  }
+
   return (
-    <button type="button" style={{ background: props.color }}>
-      {props.children}
+    <button type="button" onClick={increment} style={{ background: props.color }}>
+      {props.children} <strong>{counter}</strong>
     </button>
   );
 }
