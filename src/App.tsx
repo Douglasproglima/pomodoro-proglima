72b202d6db9ca3944d6e07df0f495c72e245c422
Douglas Lima <douglasproglima@gmail.com>
2021-03-01 21:04:32 -0300

    Create First Component Progress Bar for user's XP

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import { Button } from './components/Button';
+import { ExperienceBar } from './components/ExperienceBar';
+import './styles/global.css';
 
 function App() {
   return (
-    <>
-      <h1>Hello Pomodoro</h1>
-      <Button color="red">
-        Button 01
-      </Button>
-      <Button color="green">
-        Button 02
-      </Button>
-      <Button color="blue">
-        Button 03
-      </Button>
-    </>
+    <div className="container">
+      <ExperienceBar />
+    </div>
   );
 }
 
