02dc2d7c7e2b8d1feb272190bc0530d1eda13ec3
Douglas Lima <douglasproglima@gmail.com>
2021-03-03 20:31:32 -0300

    Finish Component Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,7 +5,10 @@ export function Profile() {
       <img src="https://github.com/douglasproglima.png" alt="Douglas Lima" />
       <div>
         <strong>Douglas Lima</strong>
-        <p>Level 1</p>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
       </div>
     </div>
   );
