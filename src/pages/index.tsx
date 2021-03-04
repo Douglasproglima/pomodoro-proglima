6ac14913f2308e512f4f47b2cef28a3e80a672ad
Douglas Lima <douglasproglima@gmail.com>
2021-03-03 21:22:29 -0300

    Define favicon and title home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,25 @@
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { CompletedChalenges } from '../components/CompletedChalenges';
+import { Countdown } from '../components/Countdown';
 import styles from '../styles/pages/Home.module.css';
-
+import Head from 'next/head';
 export default function Home() {
   return (
     <div className={styles.container} >
+      <Head>
+        <title>Início | pomodoro-proglima</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChalenges />
+          <Countdown />
         </div>
         <div>
-          
         </div>
       </section>
     </div>
