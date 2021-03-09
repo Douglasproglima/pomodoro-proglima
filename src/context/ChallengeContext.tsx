c7a9aecb8f8fb08530b516204df1234122e77003
Douglas Lima <douglasproglima@gmail.com>
2021-03-08 21:52:51 -0300

    Finish First Stape ChallengeContext

--- src/context/ChallengeContext.tsx
+++ src/context/ChallengeContext.tsx
@@ -1,11 +1,20 @@
 import { createContext, useState, ReactNode } from 'react';
+import challenges from '../../challenges.json';
+
+interface Challenge {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+}
 
 interface ChallengesContextData {
   level: number;
   currentExperience: number;
   challengesCompleted: number
+  activeChallenge: Challenge;
   levelUp: () => void;
   startNewChallenge: () => void;
+  resetChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -18,17 +27,33 @@ export function ChallengesProvider({ children } : ChallengesProviderProps) {
   const [level, setLevel] = useState(1);
   const [currentExperience, setCurrentExperience] = useState(0);
   const [challengesCompleted, setChallengesCompleted] = useState(0);
+  const [activeChallenge, setActiveChallenge] = useState(null);
 
   function levelUp() {
     setLevel(level + 1)
   }
 
   function startNewChallenge() {
-    console.log('Novo Desafio.');
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+
+    setActiveChallenge(challenge);
+  }
+
+  function resetChallenge() {
+    setActiveChallenge(null);
   }
 
   return (
-    <ChallengesContext.Provider value={{level, currentExperience, challengesCompleted, levelUp, startNewChallenge} }>
+    <ChallengesContext.Provider value={{
+      level,
+      currentExperience,
+      challengesCompleted,
+      activeChallenge,
+      levelUp,
+      startNewChallenge,
+      resetChallenge,
+    }}>
       { children }
     </ChallengesContext.Provider>
   )
