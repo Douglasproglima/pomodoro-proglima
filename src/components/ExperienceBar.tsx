cda7f32d8c994f721e152c535b9bbad19c73ecad
Douglas Lima <douglasproglima@gmail.com>
2021-03-08 21:54:36 -0300

    Implementation ChallengeContext on ExperienceBar

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,12 +1,17 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../context/ChallengeContext';
 import styles from '../styles/components/ExperienceBar.module.css';
+
 export function ExperienceBar() {
+  const { currentExperience } = useContext(ChallengesContext);
+
   return (
     <header className={styles.experienceBar}>
       <span>0 XP</span>
       <div>
         <div style={ {width: '50%'} } />
         <span className={styles.currentExperience} style={{ left: '50%' }}>
-          300 XP
+          { currentExperience } XP
         </span>
       </div>
       <span>600 XP</span>
